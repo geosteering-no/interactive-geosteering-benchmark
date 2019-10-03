d84ca76e055f5a50cf8006cc722a0393ddb1475a
Andrew Holsaeter <amh1@iris.no>
2019-10-03 16:17:06 +0200

    Use Kestrel

--- server/GameServer/Program.cs
+++ server/GameServer/Program.cs
@@ -19,6 +19,7 @@ namespace GameServer
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
                 .UseStartup<Startup>();
     }
 }
