d81858221001f8702c86b33abea4bb70ad493736
Sergey Alyaev <seal@iris.no>
2019-10-12 12:26:20 +0200

    Added return state

--- server/ServerStateInterfaces/IFullServerState.cs
+++ server/ServerStateInterfaces/IFullServerState.cs
@@ -1,10 +1,12 @@
 ﻿namespace ServerStateInterfaces
 {
-    public interface IFullServerState<in TWellPoint>
+    public interface IFullServerState<TWellPoint, TUserData>
     {
         bool AddUser(string userId);
         void RestartServer(int seed = 0);
         bool UpdateUser(string userId, TWellPoint load = default);
         bool UserExists(string userId);
+        TUserData GetUserState(string userId);
+
     }
 }
