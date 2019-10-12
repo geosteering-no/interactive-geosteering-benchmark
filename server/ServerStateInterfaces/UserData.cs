eada26d680af9f9c47f141998f7e121e692addbd
Morten Bendiksen <morten.bendikse@gmail.com>
2019-10-12 13:29:02 +0300

    add user data

---
+++ server/ServerStateInterfaces/UserData.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace ServerStateInterfaces {
+    public class UserData {
+
+        double Xtopleft { get; set; }
+        double Ytopleft { get; set; }
+        double Width { get; set; }
+        double Height { get; set; }
+
+
+        IList<double> xList { get; set; }
+
+        IList<RealizationData> realizations  { get; set; }
+    }
+
+    public class RealizationData {
+        IList<IList<double>> YLists { get; set; }
+    }
+
+    public class WellPoint {
+        double X;
+        double Y;
+        double Angle;
+    }
+}
