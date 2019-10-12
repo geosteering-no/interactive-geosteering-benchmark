77096e57650a6ccba5e7271bfdde6d2e9808168e
Sergey Alyaev <seal@iris.no>
2019-10-12 12:04:56 +0200

    Added a base server state file

---
+++ server/ServerStateInterfaces/ServerStateBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerStateInterfaces
+{
+    class ServerStateBase
+    {
+        public class FullServerStateBase<TWellPoint, TUserDataModel>
+        {
+            bool AddUser(string userId)
+            {
+                return true;
+            }
+
+            void RestartServer(int seed = 0)
+            {
+
+            }
+
+            bool UpdateUser(string userId, TWellPoint load = default)
+            {
+                return true;
+            }
+
+            bool UserExists(string userId)
+            {
+                return true;
+            }
+
+
+        }
+
+    }
+}
