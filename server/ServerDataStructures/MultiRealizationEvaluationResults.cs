984171e9cbd3a0eef7a013d92e7aaabfb4f883e6
Sergey Alyaev <seal@iris.no>
2019-10-14 18:08:16 +0200

    Added evaluation results structure

---
+++ server/ServerDataStructures/MultiRealizationEvaluationResults.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerDataStructures
+{
+    class MultiRealizationEvaluationResults
+    {
+        /// <summary>
+        /// Scores for original indexes
+        /// </summary>
+        public IList<double> RealizationScores { get; set;}
+        public IList<int> SortedIndexes { get; set; }
+    }
+}
