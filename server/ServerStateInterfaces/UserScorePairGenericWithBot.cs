faf041aa87df4eb37ee3ac1bcbb8acd3f801f01c
Sergey Alyaev <seal@iris.no>
2019-10-30 14:43:24 +0100

    Added a DDS bot implementation.

--- server/ServerStateInterfaces/UserScorePairGenericWithBot.cs
+++ server/ServerStateInterfaces/UserScorePairGenericWithBot.cs
@@ -1,26 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using ServerDataStructures;
 
 namespace ServerStateInterfaces
 {
-    public class UserScorePairGenericWithBot<TUserModel, TUserDataModel,
+    public abstract class UserScorePairGenericWithBot<TUserModel, TUserDataModel,
         TSecretState, TWellPoint,
         TUserEvaluation, TRealizationData> : UserScorePairLockedGeneric<TUserModel, TUserDataModel,
         TSecretState, TWellPoint,
         TUserEvaluation, TRealizationData> where TUserModel : IUserImplementaion<TUserDataModel, TWellPoint, TSecretState, TUserEvaluation, TRealizationData>, new()
     {
 
-        public IDssBotGeneric<TWellPoint, TRealizationData> Bot { get; set; }
+        protected IDssBotGeneric<TWellPoint, TRealizationData> Bot { get; set; }
+        protected ObjectiveEvaluatorDelegateTruth<TRealizationData, TWellPoint>.ObjectiveEvaluationFunctionSimple Objective { get; set; }
 
-        public void RunBot()
+
+        public void StartBot(
+            TSecretState secretState,
+            ObjectiveEvaluatorDelegateTruth<TRealizationData, TWellPoint>.ObjectiveEvaluationFunction evaluatorTruth,
+            TRealizationData trueRealization)
         {
             if (Bot != null)
             {
-                throw new NotImplementedException();
-
+                var thread = new Thread(() =>
+                {
+                    RunBot(secretState, evaluatorTruth, trueRealization);
+                });
+                thread.Priority = ThreadPriority.BelowNormal;
+                thread.Start();
             }
         }
+
+        protected abstract void RunBot
+            (TSecretState trueState,
+            ObjectiveEvaluatorDelegateTruth<TRealizationData, TWellPoint>.ObjectiveEvaluationFunction evaluatorTruth, 
+            TRealizationData trueRealization);
+
+
 
         public UserScorePairGenericWithBot(string userName, ObjectiveEvaluationDelegateUser<TUserDataModel, TWellPoint, TUserEvaluation>.ObjectiveEvaluationFunction EvaluatorUser, ObjectiveEvaluatorDelegateTruth<TRealizationData, TWellPoint>.ObjectiveEvaluationFunction evaluatorTruth, TRealizationData trueRealization) : base(userName, EvaluatorUser, evaluatorTruth, trueRealization) 
         {
---
+++ server/UserState/UserStatePairWithBotFull.cs
@@ -0,0 +1,43 @@
+﻿using ServerDataStructures;
+using ServerStateInterfaces;
+
+namespace UserState
+{
+    public class UserStatePairWithBotFull : UserScorePairGenericWithBot<UserState, UserData,
+        TrueModelState, WellPoint,
+        UserEvaluation, RealizationData>
+    {
+        public UserStatePairWithBotFull(string userName, ObjectiveEvaluationDelegateUser<UserData, WellPoint, UserEvaluation>.ObjectiveEvaluationFunction EvaluatorUser, ObjectiveEvaluatorDelegateTruth<RealizationData, WellPoint>.ObjectiveEvaluationFunction evaluatorTruth, RealizationData trueRealization) : base(userName, EvaluatorUser, evaluatorTruth, trueRealization)
+        {
+        }
+
+        protected override void RunBot(
+            TrueModelState trueState,
+            ObjectiveEvaluatorDelegateTruth<RealizationData, WellPoint>.ObjectiveEvaluationFunction evaluatorTruth, 
+            RealizationData trueRealization)
+        {
+            while (true)
+            {
+                var userData = UserData;
+                var userPointsCount = userData.wellPoints.Count;
+                var totalLeft = userData.TotalDecisionPoints - userPointsCount;
+                if (totalLeft <= 0)
+                {
+                    break;
+                }
+                Bot.Init(0.9,
+                    userData.Xdist,
+                    totalLeft,
+                    userData.MaxAngleChange,
+                    userData.MinInclination,
+                    Objective);
+                var lastPoint = userData.wellPoints[userPointsCount - 1];
+                var nextChoice = Bot.ComputeBestChoice(userData.realizations, lastPoint, totalLeft);
+                UpdateUser(nextChoice, trueState, evaluatorTruth, trueRealization);
+            }
+
+            StopUser(evaluatorTruth, 
+                trueRealization);
+        }
+    }
+}
