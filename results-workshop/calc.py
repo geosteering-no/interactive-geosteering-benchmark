e47ffedba95cc2339c86771a38356c5f7072d8c6
Sergey Alyaev <seal@iris.no>
2019-12-13 16:59:32 +0100

    Updated score calculator

--- results-workshop/calc.py
+++ results-workshop/calc.py
@@ -11,6 +11,7 @@ class UserScore:
         self.total_score = 0.0
         self.scores = []
         self.percents = []
+        self.games = []
 
     def game_count(self):
         return len(self.scores)
@@ -23,8 +24,9 @@ class UserScore:
 
 
 userScores = {}
-
+game_ind = 0
 for fname in files:
+    game_ind += 1
     with open(fname) as f:
         data = json.load(f)
         best = data["BestPossible"]["TrajectoryWithScore"][-1]["Score"]
@@ -32,6 +34,8 @@ for fname in files:
         for user in data["UserResults"]:
             name = user["UserName"]
             score = user["TrajectoryWithScore"][-1]["Score"]
+            # if score <= 1e-2:
+            #     continue
             score_percent = score/best*100.0
             userData = userScores.get(name, UserScore(name))
             
@@ -39,17 +43,20 @@ for fname in files:
             userData.total_score_percent += score_percent
             userData.scores.append(score)
             userData.percents.append(score_percent)
+            userData.games.append(game_ind)
             userScores[name] = userData
 
 print('len(distinct players) = {}'.format(len(userScores)))
-three_games_list = list(filter(lambda x: x.game_count() >= 2, userScores.values()))
+filtered_list = list(filter(lambda x: x.game_count() >= 3, userScores.values()))
 
-print('len(filtered) = {}'.format(len(three_games_list)))
-s = sorted(three_games_list, key=lambda x: x.total_percent_divided(), reverse=True)
+print('len(filtered) = {}'.format(len(filtered_list)))
+s = sorted(filtered_list, key=lambda x: x.total_percent_divided(), reverse=True)
+#s = sorted(filtered_list, key=lambda x: x.name.lower())
 
 i = 0
 for u in s:
     i += 1
-    print("{:2}. {:22}: games: {} score: {:6.0f}  percent: {:5.1f}".format(i, u.name, u.game_count(), u.total_score_divided(), u.total_percent_divided()))
+    #games: {}  u.game_count(),
+    print("{:2}. {:22}: score: {:6.0f}  {:5.1f}%".format(i, u.name,u.total_score, u.total_percent_divided()))
 
         
