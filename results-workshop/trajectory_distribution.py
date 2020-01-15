0186a7d9bd1ec138c94c106ced2d6b9f7fce022b
Sergey Alyaev <seal@iris.no>
2020-01-15 12:47:41 +0100

    Made a pretty figure

--- results-workshop/trajectory_distribution.py
+++ results-workshop/trajectory_distribution.py
@@ -8,6 +8,8 @@ import scipy
 import numpy as np
 from scipy import exp
 import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
+                               AutoMinorLocator)
 
 def several_steps_done(x):
     for traj in x.trajectories:
@@ -24,13 +26,17 @@ def custom_func(x):
 
 
 
-def print_results(results, i = 0):
+def print_results(results, i = 0, do_plot=False):
 
-    plt.figure(i)
+
+
     correlations = []
 
     for u in results:
         # games: {}  u.game_count(),
+
+        if do_plot:
+            plt.figure(i)
 
         score: calc.UserScore = u
         name = u.name
@@ -39,10 +45,13 @@ def print_results(results, i = 0):
             traj = trajs[j]
             xs = list(map(lambda x: x["wellPoint"]["X"], traj))
             ys = list(map(lambda x: -x["wellPoint"]["Y"], traj))
-            plt.plot(xs, ys, label=str(j))
-        plt.axis([0, 350, -40, 0])
-        plt.legend()
-        plt.title(name)
+            if do_plot:
+                plt.plot(xs, ys, label=str(j))
+
+        if do_plot:
+            plt.axis([0, 350, -40, 0])
+            plt.legend()
+            plt.title(name)
 
         # computing correlations
         ys0 = list(map(lambda x: x["wellPoint"]["Y"], trajs[0]))
@@ -88,57 +97,105 @@ results = sorted(results, key=lambda x: x.name.lower(), reverse=False)
 print("Total qualified ", len(results))
 print_results(results)
 
+
+def dss_test(x):
+    return x.all_diffs[2] < 1e-9
+
+
 def simple_consistent_test(x):
-    return x.all_diffs[2] <= 0.5
+    return not dss_test(x) and x.all_diffs[2] <= 0.5
+
 
 def comparatively_consistent_test(x):
-    return (not simple_consistent_test(x)) \
-        and x.all_diffs[2] <= x.all_diffs[0]-x.std_diff \
-        and x.all_diffs[2] <= x.all_diffs[1]-x.std_diff
+    return (not dss_test(x) and not simple_consistent_test(x)) \
+        and x.all_diffs[2] <= x.all_diffs[0]-x.std_diff*2 \
+        and x.all_diffs[2] <= x.all_diffs[1]-x.std_diff*2
 
 
 def sort_scores(results):
     return sorted(results, key=lambda x: x.total_percent_divided(), reverse=True)
 
-def make_plot(results, start_index, label=''):
-    plt.figure(100)
+def make_plot(results, start_index, label='', plot_number=None):
+    if plot_number != None:
+        plt.figure(plot_number)
+    else:
+        plt.figure()
     xs = np.array(range(start_index,start_index+len(results)))
     ys = np.array(list(map(lambda x: x.total_percent_divided(), results)))
     plt.bar(xs, ys, label=label)
 
 
-plt.figure(100)
-plt.bar([0.5], [100], align='edge', color='gray', label='Correct layer guessed randomly',width=len(results)/8)
+plot_number = 1000
+
+
+
+i = 0
+
+my_fig = plt.figure(plot_number)
+my_ax : plt.Axes = my_fig.add_subplot(1,1,1)
+plt.xticks(range(1, len(results)+1, 2))
+plt.grid(True, which='major', axis='y', linewidth='1')
+plt.grid(True, which='minor', axis='y', linewidth='0.5')
+my_ax.set_axisbelow(True)
+# Make a plot with major ticks that are multiples of 20 and minor ticks that
+# are multiples of 5.  Label major ticks with '%d' formatting but don't label
+# minor ticks.
+my_ax.yaxis.set_major_locator(MultipleLocator(10))
+my_ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
+
+# For the minor ticks, use no labels; default NullFormatter.
+my_ax.yaxis.set_minor_locator(MultipleLocator(2))
+#my_ax.tick_params('major', width=2)
+
+
+dss_users = sort_scores(list(filter(dss_test, results)))
+print("consistent_users")
+print_results(dss_users, i+1)
+make_plot(dss_users, 1, label="DSS bot user", plot_number=plot_number)
+
+i += len(dss_users)
+
+plt.figure(plot_number)
+# a bit hard-coded
+plt.bar([0.5 + len(dss_users)], [100], align='edge', color='gray', alpha=0.3,
+        label='Correct layer guessed randomly',
+        width=(len(results)-len(dss_users))/8)
 
 consistent_users = sort_scores(list(filter(simple_consistent_test, results)))
 print("consistent_users")
-print_results(consistent_users, 1)
-make_plot(consistent_users, 1, label="Consistent users")
+print_results(consistent_users, i+1)
+make_plot(consistent_users, i+1, label="Consistent users", plot_number=plot_number)
 
+i += len(consistent_users)
 
 comparatively_consistent_users = sort_scores(list(filter(comparatively_consistent_test,
                                         results)))
 
 print("comparatively_consistent_users")
-print_results(comparatively_consistent_users, len(consistent_users)+1)
-make_plot(comparatively_consistent_users, len(consistent_users)+1,
-          label="Relatively consistent users")
+print_results(comparatively_consistent_users, i+1, do_plot=True)
+make_plot(comparatively_consistent_users, i+1,
+          label="Relatively consistent users", plot_number=plot_number)
 
+i += len(comparatively_consistent_users)
 
 non_consistent_users = sort_scores(list(filter(lambda x:
+                                               (not dss_test(x))
+                                   and
                                    (not comparatively_consistent_test(x))
                                    and
                                    (not simple_consistent_test(x)),
                                    results)))
 
 print("non_consistent_users")
-print_results(non_consistent_users, len(consistent_users)+len(comparatively_consistent_users)+1)
-make_plot(non_consistent_users, len(consistent_users)+len(comparatively_consistent_users)+1,
-          label="Other users")
+print_results(non_consistent_users, i+1)
+make_plot(non_consistent_users, i+1,
+          label="Other users", plot_number=plot_number)
 
-# hard-coded
-ys = np.array(list(map(lambda x: x.total_percent_divided(), consistent_users)))
+# line for indication of best DSS
+ys = np.array(list(map(lambda x: x.total_percent_divided(), dss_users)))
 plt.plot([0, len(results)], [ys[0], ys[0]])
+
+
 plt.legend()
 
 plt.show()
