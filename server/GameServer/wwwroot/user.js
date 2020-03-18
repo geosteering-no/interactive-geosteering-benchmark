f3d4b4974ab8b61d9b84139d4d08ce82c73f9cbf
Andrew Holsaeter <hols@norceresearch.no>
2020-03-18 11:17:10 +0100

    Copy to clipboard
    Closes #127

--- server/GameServer/wwwroot/user.js
+++ server/GameServer/wwwroot/user.js
@@ -671,6 +671,37 @@ function detectGameStateAndUpdateButton() {
   }
 }
 
+function copyToClipboard(){
+  var str = "http://game.geosteering.no";
+  
+  const el = document.createElement('textarea');  // Create a <textarea> element
+  el.value = str;                                 // Set its value to the string that you want copied
+  el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+  el.style.position = 'absolute';                 
+  el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+  document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+
+  const selected =            
+    document.getSelection().rangeCount > 0        // Check if there is any content selected previously
+      ? document.getSelection().getRangeAt(0)     // Store selection if found
+      : false;                                    // Mark as false to know no selection existed before
+  el.select();                                    // Select the <textarea> content
+  document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+  document.body.removeChild(el);                  // Remove the <textarea> element
+  if (selected) {                                 // If a selection existed before copying
+    document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+    document.getSelection().addRange(selected);   // Restore the original selection
+  }
+  
+  // Show notifcation
+  var snackbar = $("#snackbar");
+
+  snackbar.addClass("show");
+
+   // After 3 seconds, remove the show class from DIV
+   setTimeout(function(){ snackbar.removeClass("show"); }, 3000);
+}
+
 function formatModalShareLinks(percentile){
   /* 
   Loops through all share-btns and replaces their anchors href placeholder strings w/ ranking injected Formats social media share urls 
@@ -693,8 +724,6 @@ function formatModalShareLinks(percentile){
     var text =  anchor.attr('href');
     var new_text = text.replace(/SHARE_URL/g, share_url).replace(/SHARE_TEXT/g, share_text); // wrapping in '/ /g' finds all string occurances
     var formatted_text = new_text.replace(/ /g, "%20");
-    
-    console.log("====================\n", text, "\n=====\n", formatted_text, "\n=============================");
 
     anchor.attr('href', formatted_text);
   });
@@ -713,6 +742,7 @@ function endGameModal(value, percentile) {
 
   // Set up buttons
   $('#continuebtn').off('click').on('click',commitNewGame);
+  $("#copyToClipboard").click(copyToClipboard);
 
   // Show modal
   $('#endGameModal').modal('show');
